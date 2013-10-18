1c541254940450bf94fb58ad1b14dd5d0655882f
Chris Lee <sihrc.c.lee@gmail.com>
2013-10-17 22:30:26 -0400

    Able to include HEADER but errors

--- src/watchdog.c
+++ src/watchdog.c
@@ -7,7 +7,7 @@
  *Watchdog
 
 */
-
+#include "api.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -15,8 +15,8 @@
 #define F_CPU (1000000 UL) 			 //Setting the clock speed in delay.h
 #define maxDataLength 8
 
-bool BMSDeath = false;
-bool MCDeath = false;
+int BMSDeath = 0;
+int MCDeath = 0;
 int BMSShunt = 0;
 
 int main(void){
@@ -24,7 +24,7 @@ int main(void){
 
 	//Set up the WatchDog Death Timer
 	TCCR1B |= 1<<CS11 | 1<<CS10;	 //Divide by 64
-	OCR1A = 15624 * 4; 			 	 //Watchdog death timer every 2 seconds
+	OCR1A = 15624 * 2; 			 	 //Watchdog death timer every 2 seconds
 	TCCR1B |= 1<<WGM12; 		 	 //Sets death timer in CTC mode
 	TIMSK1 |= 1<<OCIE1A;		 	 //Enables timer compare interrupt
 
@@ -43,11 +43,11 @@ int main(void){
 ISR(TIMER1_COMPA_vect)  //Die Die Die Timer ISR
 {
 	//Check BMS and MC status
-	if (BMSDeath or MCDeath){
+	if (BMSDeath || MCDeath){
 		//Send kill-signal
 		PORTD |= (1<<PD0);  		 //Setting Pin0 to high	
-		BMSDeath = false;
-		MCDeath = false;
+		BMSDeath = 0;
+		MCDeath = 0;
 	}
 	
 	if (BMSShunt > 10){
@@ -56,9 +56,9 @@ ISR(TIMER1_COMPA_vect)  //Die Die Die Timer ISR
 	}
 }
 
-void handleCANmsg(uint8_t destID, uint8_t msgID, char msg[], uint8_t msgLen) {
+void handleCANmsg(uint8_t destID, uint8_t msgID, char msg[], uint8_t msgLen){
     if (msgID == MSG_voltagelow)
-    	BMSDeath = true;
+    	BMSDeath = 1;
     
     if (msgID == MSG_shunting)
     	BMSShunt++;
